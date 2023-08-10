cb5b57a2027349c54c84f717b7ebdbc48af5b5a6
christian <christian.charon@mailbox.org>
2023-08-10 18:08:49 +0200

    cleanup

--- test/deploy.sh
+++ test/deploy.sh
@@ -3,11 +3,9 @@
 echo "received deployment request ${DEPLOY_ID} for ${DEPLOY_IMAGE}"
 
 if [ "${DEPLOY_IMAGE}" == "ccharon/echoip" ] ; then
-
-    echo "..."
+    echo "deployment started"
     sleep 10
-
-    echo "fertig"
+    echo "deployment finished"
 fi
 
 exit 0
