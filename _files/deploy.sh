c80d58c0daa713c44bc8850abe7953c6c8d45869
ccharon <christian.charon@mailbox.org>
2026-05-24 19:40:24 +0200

    Enhance deploy script for soundboard deployment
    Add deployment logic for soundboard with temporary directory and rsync.

--- _files/deploy.sh
+++ _files/deploy.sh
@@ -10,8 +10,23 @@ if [ "${WEBHOOK_PARAM}" == "ccharon/echoip" ] ; then
         docker compose rm -f
         docker pull ccharon/echoip:latest
         docker compose up -d
-
+        
         echo "Done deploying ${WEBHOOK_PARAM}"
+fi
+
+if [ "${WEBHOOK_PARAM}" == "soundboard" ] ; then
+        # before first soundboard deploy webhook has to be allowed to write the target
+        # one time manually run: 'chown -R webhook:webhook /var/www/sound.erdferkel.eu'
+        
+        TMPDIR=$(mktemp -d)
+        trap 'rm -rf "$TMPDIR"' EXIT
+        
+        curl -sL https://github.com/ccharon/soundboard/archive/refs/heads/dist.tar.gz \
+                | tar -xz -C "$TMPDIR" --strip-components=1
+        
+        rsync -a --delete --chmod=D755,F644 "$TMPDIR/" /var/www/sound.erdferkel.eu/
+        
+        echo "Done deploying soundboard"
 fi
 
 exit 0
